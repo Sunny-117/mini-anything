09944d1b482f8de6aec6f44ddd77c93880491bbd
付志强 <zhiqiangfu6@gmail.com>
2023-01-13 15:58:01 +0800

    feat: react-redux example finish

--- packages/mini-react-redux/src/Counter.js
+++ packages/mini-react-redux/src/Counter.js
@@ -1,16 +1,14 @@
 import React from 'react'
-import { asyncDecrease, asyncIncrease, increase, decrease } from "./store/action/counter"
-import { connect } from "react-redux"
+import { increase, decrease } from "./store/action/counter"
+import { connect } from "./react-redux"
 //展示组件
 function Counter(props) {
     return (
         <div>
             <h1>{props.number}</h1>
             <p>
-                <button onClick={props.onAsyncDecrease}> 异步减 </button>
                 <button onClick={props.onDecrease}> 减 </button>
                 <button onClick={props.onIncrease}> 加 </button>
-                <button onClick={props.onAsyncIncrease}> 异步加 </button>
             </p>
         </div>
     )
@@ -27,10 +25,8 @@ function mapStateToProps(state, ownProps) {
 }
 
 const creators = {
-    onAsyncDecrease: asyncDecrease,
     onDecrease: decrease,
     onIncrease: increase,
-    onAsyncIncrease: asyncIncrease
 };
 
 export default connect(mapStateToProps, creators)(Counter)
