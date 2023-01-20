0ded233f1e9eb61edb1ef811aebd59bfe4281df0
付志强 <zhiqiangfu6@gmail.com>
2023-01-20 17:20:49 +0800

    feat: remove index

---
+++ packages/mini-mobx/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports  = {
+  entry:"./src/index.js",
+  output:{
+    filename:'bundle.js',
+    path:require('path').resolve(__dirname,'dist')
+  },
+  mode:'development',
+  module:{
+    rules:[
+      {test:/\.js$/,use:{
+        loader:'babel-loader',
+        options:{
+          presets:['env','react','stage-0'],
+          plugins:['transform-decorators-legacy']
+        }
+      }}
+    ]
+  }
+}
