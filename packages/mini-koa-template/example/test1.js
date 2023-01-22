7c392f2a15f9c21434e884e016a3678fbce6a9e4
付志强 <zhiqiangfu6@gmail.com>
2023-01-22 13:58:22 +0800

    feat: koa test

--- packages/mini-koa-template/example/test1.js
+++ packages/mini-koa-template/example/test1.js
@@ -1,9 +1,8 @@
 import Koa from './lib/mini.esm.js';
-let app = new Koa();
-app.use((req, res) => {
-  console.dir(res)
-  res.end('hello zfpx');
+const app = new Koa();
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
 });
-app.listen(3000, () => {
-  console.log('[ listening 3000 ] >')
-});
+
+app.listen(6789);
--- packages/mini-koa-template/example/test2.js
+++ packages/mini-koa-template/example/test2.js
@@ -1,10 +1,10 @@
-let Koa = require('./koa/application');
+import Koa from './lib/mini.esm';
 let app = new Koa();
 app.use((ctx) => {
   console.log(ctx.req.path);
   console.log(ctx.request.req.path);
   console.log(ctx.request.path);
   console.log(ctx.path); // ctx会代理ctx.request上的属性
-  ctx.body = 'zfpx';
+  ctx.body = 'sunny';
 });
-app.listen(3000);
+app.listen(6789);
